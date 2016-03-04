be8b29667e5ec155d3c0823c235958b3777d5547
Leonardo Villani <informatica@villani.eti.br>
2016-03-04 15:32:52 -0300

    Alteração da página inicial do projeto.

--- imobiliaria/index.php
+++ imobiliaria/index.php
@@ -1,1 +1,11 @@
-<?php
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Projeto Imobiliária</title>
+</head>
+<body>
+	<h1>Projeto Imobiliária</h1>
+	<p>Alterações no projeto.</p>
+</body>
+</html>
